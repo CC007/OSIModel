ce4fc3bd31d78beec43f9b77c721848c5c1029da
CC007 <Coolcat_the_best2@hotmail.com>
2014-08-10 14:58:46 +0200

    javadoc physical layer

--- src/main/java/com/github/cc007/osimodel/physicalLayer/PhysicalLayerProtocol.java
+++ src/main/java/com/github/cc007/osimodel/physicalLayer/PhysicalLayerProtocol.java
@@ -3,17 +3,30 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.github.cc007.osimodel.physicalLayer;
 
 import com.github.cc007.osimodel.datalinkLayer.DatalinkLayerProtocol;
 import java.io.Serializable;
 
 /**
+ * For simulation purposes only, it will simulate how data is transferred from
+ * one point to another.
  *
  * @author Rik
  */
-public interface PhysicalLayerProtocol extends Serializable{
+public interface PhysicalLayerProtocol extends Serializable {
+
+    /**
+     * set the content, which follows a network layer protocol
+     *
+     * @param frame
+     */
     public void setFrame(DatalinkLayerProtocol frame);
+
+    /**
+     * get the content, which follows a network layer protocol
+     *
+     * @return
+     */
     public DatalinkLayerProtocol getFrame();
 }
