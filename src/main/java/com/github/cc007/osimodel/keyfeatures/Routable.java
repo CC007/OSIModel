4b0eeb2ac0cbabb6dacb9b18496cf7635f8c13c1
CC007 <Coolcat_the_best2@hotmail.com>
2014-09-27 22:37:24 +0200

    restructuring and bug fixes

--- src/main/java/com/github/cc007/osimodel/keyfeatures/Routable.java
+++ src/main/java/com/github/cc007/osimodel/keyfeatures/Routable.java
@@ -5,18 +5,38 @@
  */
 package com.github.cc007.osimodel.keyfeatures;
 
+import com.github.cc007.osimodel.Address;
+
 /**
  *
  * @author Rik
  */
 public interface Routable {
 
-    public byte[] getSource();
+    /**
+     * Get the source of the data transfer
+     *
+     * @return the address of the source
+     */
+    public Address getSource();
 
+    /**
+     * Set the source of the data transfer to the addres of the current device
+     */
     public void setSource();
 
-    public byte[] getDestination();
+    /**
+     * Get the destination of the data transfer
+     *
+     * @return the address of the destination
+     */
+    public Address getDestination();
 
-    public void setDestination(byte[] destination);
+    /**
+     * Set the source of the data transfer
+     *
+     * @param destination the address of the destination
+     */
+    public void setDestination(Address destination);
 
 }
