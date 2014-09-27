baa7898f1211e9cd21c41a8d7dd3f3a3439db307
CC007 <Coolcat_the_best2@hotmail.com>
2014-09-27 23:48:50 +0200

    restructuring and bug fixes

--- src/main/java/com/github/cc007/osimodel/protocols/physicalLayer/PhysicalLayerProtocol.java
+++ src/main/java/com/github/cc007/osimodel/protocols/physicalLayer/PhysicalLayerProtocol.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.github.cc007.osimodel.physicalLayer;
+package com.github.cc007.osimodel.protocols.physicalLayer;
 
 import com.github.cc007.osimodel.keyfeatures.Collapsable;
 import com.github.cc007.osimodel.protocols.datalinkLayer.DatalinkLayerProtocol;
